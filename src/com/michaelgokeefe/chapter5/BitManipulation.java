16cefbbef30dd70596a396df71fa6ae942ccb30a
MichaelGeraldOKeefe <mgok@uw.edu>
2017-07-24 14:54:49 -0700

    5.2 completed - print binary. And bit set, clear, get, update

--- src/com/michaelgokeefe/chapter5/BitManipulation.java
+++ src/com/michaelgokeefe/chapter5/BitManipulation.java
@@ -5,6 +5,9 @@ package com.michaelgokeefe.chapter5;
  */
 public class BitManipulation {
 
+    public static final String ERROR = "ERROR";
+
+    // 5.1
     public static int insertBits(int N, int M, int i, int j) {
         if (j - i > 32) { throw new IllegalArgumentException("Only 32 bits supported."); }
 
@@ -21,5 +24,62 @@ public class BitManipulation {
 
         // Insert M into N
         return M | N;
+    }
+
+    // 5.2
+    public static String toBinaryString(double num) {
+        if (num >= 1 || num <= 0) {
+            return ERROR;
+        }
+
+        StringBuilder binary = new StringBuilder();
+        binary.append(".");
+
+        while (num > 0) {
+            if (binary.length() >= 32) {
+                return ERROR;
+            }
+
+            double r = num * 2;
+            if (r >= 1) {
+                binary.append(1);
+                num = r - 1;
+            } else {
+                binary.append(0);
+                num = r;
+            }
+        }
+        return binary.toString();
+    }
+
+    public static boolean getBit(int num, int index) {
+        int bit = 1 << index;
+        return (bit & num) != 0;
+    }
+
+    public static int setBit(int num, int index) {
+        int bit = 1 << index;
+        return (bit | num);
+    }
+
+    public static int clearBit(int num, int index) {
+        int mask = ~(1 << index);
+        return (num & mask);
+    }
+
+    public static int clearMSBthroughI(int num, int index) {
+        int mask = (1 << index) - 1;
+        return (num & mask);
+    }
+
+    public static int clear0BitThroughI(int num, int index) { // -1 is a sequence of all 1's
+        int mask = -1 << index + 1;
+        return (num & mask);
+    }
+
+    public static int updateBit(int num, int index, boolean bitIs1) {
+        int value = bitIs1 ? 1 : 0;
+        int mask = ~(1 << index);
+        return (num & mask) | (value << index);
     }
 }
--- test/com/michaelgokeefe/chapter5/BitManipulationTest.java
+++ test/com/michaelgokeefe/chapter5/BitManipulationTest.java
@@ -10,9 +10,73 @@ import static org.junit.Assert.*;
  */
 public class BitManipulationTest {
 
+    // 5.1
     @Test
     public void insertBits() throws Exception {
         assertEquals(12, BitManipulation.insertBits(14, 2, 1, 2));
     }
 
+    // 5.2
+    @Test
+    public void toBinaryString() throws Exception {
+        assertEquals(".1", BitManipulation.toBinaryString(.5));
+        assertEquals(".01", BitManipulation.toBinaryString(.25));
+        assertEquals(BitManipulation.ERROR, BitManipulation.toBinaryString(.72));
+    }
+
+    // get bit
+    @Test
+    public void getBit_AllZeros() {
+        assertEquals(false, BitManipulation.getBit(0, 11));
+    }
+
+    @Test
+    public void getBit_OverThirtyTwoShifts() {
+        assertEquals(false, BitManipulation.getBit(12345, 33));
+    }
+
+    @Test
+    public void getBit_ExpectedTrues() {
+        assertEquals(true, BitManipulation.getBit(1, 32));
+    }
+
+    // set bit
+    @Test
+    public void setBit_ZeroToOne() {
+        assertEquals(1, BitManipulation.setBit(0, 0));
+    }
+
+    @Test
+    public void setBit_ThreeToSeven() {
+        assertEquals(7, BitManipulation.setBit(3, 2));
+    }
+
+    // clear bit
+    @Test
+    public void clearBit_OneToZero() {
+        assertEquals(0, BitManipulation.clearBit(1, 0));
+    }
+
+    @Test
+    public void clearBit_SevenToThree() {
+        assertEquals(3, BitManipulation.clearBit(7, 2));
+    }
+
+    // clear MSB through index
+    @Test
+    public void clearMSBthroughI_FifteenToThree() {
+        assertEquals(3, BitManipulation.clearMSBthroughI(15, 2));
+    }
+
+    // clear 0 bit through index
+    @Test
+    public void clear0BitThroughI_SevenToFour() {
+        assertEquals(4, BitManipulation.clear0BitThroughI(7, 1));
+    }
+
+    // update bit
+    @Test
+    public void updateBit_ZeroToOne() {
+        assertEquals(1, BitManipulation.updateBit(0, 0, true));
+    }
 }
