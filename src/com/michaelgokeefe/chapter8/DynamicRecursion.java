4b34de9ec87963a2a8b57cdb9aa6aeeb64d1b826
MichaelGeraldOKeefe <mgok@uw.edu>
2017-07-29 17:08:06 -0700

    Implemented and tested 8.2 - Finding path on grid - dynamic recursion

--- src/com/michaelgokeefe/chapter8/DynamicRecursion.java
+++ src/com/michaelgokeefe/chapter8/DynamicRecursion.java
@@ -1,9 +1,17 @@
 package com.michaelgokeefe.chapter8;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Stack;
+
 /**
  * Created by Michael on 2/27/17.
  */
 public class DynamicRecursion {
+
+    private static final char RIGHT = 'R';
+    private static final char DOWN = 'D';
 
     // 8.1
     public static int numWays(int n) {
@@ -26,5 +34,39 @@ public class DynamicRecursion {
         memo[n] = numWays(n - 3, memo) + numWays(n - 2, memo) + numWays(n - 1, memo);
 
         return memo[n];
+    }
+
+    // In any path finding - use breadth before depth ***
+    public static ArrayList<Point> findRobotPath(boolean[][] grid) {
+        if (grid == null || grid.length == 0) {
+            throw new IllegalArgumentException("Empty or null grid.");
+        }
+        ArrayList<Point> path = new ArrayList<>();
+        HashSet<Point> failedPoints = new HashSet<>();
+        findRobotPath(grid, path, grid.length -1, grid[0].length - 1, failedPoints);
+        return path;
+    }
+
+    private static boolean findRobotPath(boolean[][] grid, ArrayList<Point> path, int row, int col, HashSet<Point> failedPoints) {
+
+        // Index OOB or off limits
+        if (row < 0 || col < 0 || !grid[row][col]) {
+            return false;
+        }
+
+        Point point = new Point(row, col);
+
+        if (failedPoints.contains(point)) {
+            return false;
+        }
+
+        boolean atOrigin = (row == 0) && (col == 0);
+
+        if (atOrigin || findRobotPath(grid, path, row - 1, col, failedPoints) || findRobotPath(grid, path, row, col - 1, failedPoints)) {
+            path.add(point);
+            return true;
+        }
+        failedPoints.add(point); // by caching failed points, we reduce the runtime from O(2^r+c) to O(rc) - aka searching each index of the grid
+        return false;
     }
 }
--- test/com/michaelgokeefe/chapter8/DynamicRecursionTest.java
+++ test/com/michaelgokeefe/chapter8/DynamicRecursionTest.java
@@ -3,15 +3,71 @@ package com.michaelgokeefe.chapter8;
 import com.michaelgokeefe.chapter8.DynamicRecursion;
 import org.junit.Test;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Michael on 2/27/17.
  */
 public class DynamicRecursionTest {
+    //8.1
     @Test
     public void numWays() throws Exception {
         assertEquals(7, DynamicRecursion.numWays(4));
+    }
+
+    // 8.2
+
+    private ArrayList<Point> exp = new ArrayList<>();
+
+    @Test
+    public void findPath_EmptyPathOnOneByOneGrid() {
+        boolean[][] grid = new boolean[1][1];
+        assertEquals(exp, DynamicRecursion.findRobotPath(grid));
+    }
+
+    @Test
+    public void findPath_NoLimitsTwoByTwoGrid() {
+        boolean[][] grid = new boolean[][] {{true, true}, {true, true}};
+        exp.add(new Point(0, 0));
+        exp.add(new Point(0, 1));
+        exp.add(new Point(1,1));
+        assertEquals(exp, DynamicRecursion.findRobotPath(grid));
+    }
+
+    @Test
+    public void findPath_OnlyDownNoLimits() {
+        boolean[][] grid = new boolean[][] {{true}, {true}};
+        exp.add(new Point(0, 0));
+        exp.add(new Point(1, 0));
+        assertEquals(exp, DynamicRecursion.findRobotPath(grid));
+    }
+
+    @Test
+    public void findPath_OnlyRightNoLimits() {
+        boolean[][] grid = new boolean[][] {{true, true}};
+        exp.add(new Point(0, 0));
+        exp.add(new Point(0, 1));
+        assertEquals(exp, DynamicRecursion.findRobotPath(grid));
+    }
+
+    @Test
+    public void findPath_ThreeByThreeGridWithMiddleLimit() {
+        boolean[][] grid = {{true, true, true}, {true, false, true}, {false, true, true}};
+        exp.add(new Point(0, 0));
+        exp.add(new Point(0, 1));
+        exp.add(new Point(0, 2));
+        exp.add(new Point(1, 2));
+        exp.add(new Point(2, 2));
+        assertEquals(exp, DynamicRecursion.findRobotPath(grid));
+    }
+
+    @Test
+    public void findPath_NoPath() {
+        boolean[][] grid = {{true, true, true}, {true, false, true}, {false, true, false}};
+        assertEquals(exp, DynamicRecursion.findRobotPath(grid));
     }
 
 }
