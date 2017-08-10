b4be06bc80322336c513fc8effc8934c6036c97b
MichaelGeraldOKeefe <mgok@uw.edu>
2017-08-10 12:15:45 -0700

    Implemented and tested merge sort

--- src/com/michaelgokeefe/chapter10/SortingSearching.java
+++ src/com/michaelgokeefe/chapter10/SortingSearching.java
@@ -4,6 +4,47 @@ package com.michaelgokeefe.chapter10;
  * Created by Michael on 2/27/17.
  */
 public class SortingSearching {
+    // merge sort practice
+    public static void mergeSort(int[] array) {
+        mergeSort(array, new int[array.length], 0, array.length - 1);
+    }
+
+    private static void mergeSort(int[] array, int[] helper, int low, int high) {
+        if (low < high) {
+            int middle = (low + high) / 2;
+            mergeSort(array, helper, low, middle);
+            mergeSort(array, helper, middle + 1, high);
+            merge(array, helper, low, middle, high);
+        }
+    }
+
+    private static void merge(int[] array, int[] helper, int low, int middle, int high) {
+        for (int i = low; i <= high; i++) {
+            helper[i] = array[i];
+        }
+
+        int leftHelper = low;
+        int rightHelper = middle + 1;
+        int current = low;
+
+        while (leftHelper <= middle && rightHelper <= high) {
+            if (helper[leftHelper] <= helper[rightHelper]) {
+                array[current] = helper[leftHelper];
+                leftHelper++;
+            } else {
+                array[current] = helper[rightHelper];
+                rightHelper++;
+            }
+            current++;
+        }
+
+        int remaining = middle - leftHelper;
+        for (int i = 0; i <= remaining; i++) {
+            array[current + i] = helper[leftHelper + i];
+        }
+    }
+
+    // 10.1
     public static void merge(int[] a, int[] b, int endA) {
 
         if (a == null || b == null || endA < 0) {
--- test/com/michaelgokeefe/chapter10/SortingSearchingTest.java
+++ test/com/michaelgokeefe/chapter10/SortingSearchingTest.java
@@ -3,18 +3,66 @@ package com.michaelgokeefe.chapter10;
 import com.michaelgokeefe.chapter10.SortingSearching;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Michael on 2/27/17.
  */
 public class SortingSearchingTest {
+    private int[] expected;
+    private int[] actual;
+
+    // merge sort practice
+    @Test
+    public void mergeSort_SortEmptyArray() {
+        setUpExpectedAndActual(new int[0]);
+        SortingSearching.mergeSort(actual);
+        testExpectedAndActual();
+    }
+
+    @Test
+    public void mergeSort_SortSingleElementArray() {
+        int[] array = new int[]{1};
+        setUpExpectedAndActual(array);
+        SortingSearching.mergeSort(actual);
+        testExpectedAndActual();
+    }
+
+    @Test
+    public void mergeSort_SortTwoElementArray() {
+        int[] array = new int[]{2, 1};
+        setUpExpectedAndActual(array);
+        SortingSearching.mergeSort(actual);
+        testExpectedAndActual();
+    }
+
+    @Test
+    public void mergeSort_SortLargeArrayOddNumElements() {
+        int[] array = new int[] { -1, 3, 435, 56, 54, 33, 75, -45, 67, 8665, 33 };
+        setUpExpectedAndActual(array);
+        SortingSearching.mergeSort(actual);
+        testExpectedAndActual();
+    }
+
+    // 10.1
     @Test
     public void merge() throws Exception {
-        int[] a = new int[] { 1, 5, 10, 17, 19, 0, 0, 0 };
-        int[] b = new int[] { 1, 11, 27 };
+        int[] a = new int[]{1, 5, 10, 17, 19, 0, 0, 0};
+        int[] b = new int[]{1, 11, 27};
         SortingSearching.merge(a, b, 4);
-        assertArrayEquals(new int[] { 1, 1, 5, 10, 11, 17, 19, 27 }, a);
+        assertArrayEquals(new int[]{1, 1, 5, 10, 11, 17, 19, 27}, a);
+    }
+
+    private void setUpExpectedAndActual(int[] arrayToSort) {
+        expected = arrayToSort;
+        actual = Arrays.copyOf(expected, expected.length);
+        Arrays.sort(expected);
+    }
+
+    private void testExpectedAndActual() {
+        assertArrayEquals(expected, actual);
     }
 
 }
