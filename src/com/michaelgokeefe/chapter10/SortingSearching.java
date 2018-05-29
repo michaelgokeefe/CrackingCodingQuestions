6968507fea51d66f2be882786d241e904a6293bd
Michael O'Keefe <mgok@uw.edu>
2018-05-29 11:32:48 -0700

    Merge Sort

--- src/com/michaelgokeefe/chapter10/SortingSearching.java
+++ src/com/michaelgokeefe/chapter10/SortingSearching.java
@@ -4,7 +4,7 @@ package com.michaelgokeefe.chapter10;
  * Created by Michael on 2/27/17.
  */
 public class SortingSearching {
-    public static void merge(int[] a, int[] b, int endA) {
+    public static void mergeArrays(int[] a, int[] b, int endA) {
 
         if (a == null || b == null || endA < 0) {
             throw new IllegalArgumentException();
@@ -24,6 +24,47 @@ public class SortingSearching {
                 bIndex--;
             }
             mergeIndex--;
+        }
+    }
+
+    // merge sort - 2018
+
+    public static void mergesort(int[] array) {
+        int[] helper = new int[array.length];
+        mergesort(array, helper, 0, array.length - 1);
+    }
+
+    private static void mergesort(int[] array, int[] helper, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            mergesort(array, helper, low, mid);
+            mergesort(array, helper, mid + 1, high);
+            merge(array, helper, low, mid, high);
+        }
+    }
+
+    private static void merge(int[] array, int[] helper, int low, int mid, int high) {
+        // Copy sorted halves to helper
+        for (int i = low; i <= high; i++) {
+            helper[i] = array[i];
+        }
+
+        int helperLow = low;
+        int helperHigh = mid + 1;
+        int current = low;
+
+        while (helperLow <= mid && helperHigh <= high) {
+            if (helper[helperLow] <= helper[helperHigh]) {
+                array[current] = helper[helperLow++];
+            } else {
+                array[current] = helper[helperHigh++];
+            }
+            current++;
+        }
+
+        int remaining = mid - helperLow;
+        for (int i = 0; i <= remaining; i++) {
+            array[current + i] = helper[helperLow + i];
         }
     }
 }
--- test/com/michaelgokeefe/chapter10/SortingSearchingTest.java
+++ test/com/michaelgokeefe/chapter10/SortingSearchingTest.java
@@ -1,7 +1,8 @@
 package com.michaelgokeefe.chapter10;
 
-import com.michaelgokeefe.chapter10.SortingSearching;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -13,8 +14,43 @@ public class SortingSearchingTest {
     public void merge() throws Exception {
         int[] a = new int[] { 1, 5, 10, 17, 19, 0, 0, 0 };
         int[] b = new int[] { 1, 11, 27 };
-        SortingSearching.merge(a, b, 4);
+        SortingSearching.mergeArrays(a, b, 4);
         assertArrayEquals(new int[] { 1, 1, 5, 10, 11, 17, 19, 27 }, a);
     }
+
+    // merge sort
+    @Test
+    public void testEmpty() {
+        int[] a = new int[] {};
+        SortingSearching.mergesort(a);
+        assertArrayEquals(new int[] {}, a);
+    }
+
+    @Test
+    public void testSingleElement() {
+        int[] a = new int[] {1};
+        SortingSearching.mergesort(a);
+        assertArrayEquals(new int[] {1}, a);
+    }
+
+    @Test
+    public void testEvenElements() {
+        int[] a = new int[] {10, 9, 15, 18, -1, 11, 11, 7};
+        int[] answer = Arrays.copyOf(a, a.length);
+        Arrays.sort(answer);
+        SortingSearching.mergesort(a);
+        assertArrayEquals(answer, a);
+    }
+
+    @Test
+    public void testOddElements() {
+        int[] a = new int[] {27, 8, -2, -5, -1};
+        int[] answer = Arrays.copyOf(a, a.length);
+        Arrays.sort(answer);
+        SortingSearching.mergesort(a);
+        assertArrayEquals(answer, a);
+    }
+
+
 
 }
